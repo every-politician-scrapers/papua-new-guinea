c2ed44a036ba97a5456829649801ee2bbadc794e
Tony Bowden <tony@tmtm.com>
2022-08-22 07:51:42 +0300

    compare Governors General

---
+++ sources/wpoffce/governor-general/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no img name start end].freeze
+    end
+
+    def raw_start
+      start_cell.children.map(&:text).map(&:tidy).reject(&:empty?).join(' ').tidy
+    end
+
+    def raw_end
+      end_cell.children.map(&:text).map(&:tidy).reject(&:empty?).join(' ').tidy
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
